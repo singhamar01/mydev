e50f3ec16501ec700487e1db8a047fb145837197
nova-miniPC <singhamar01@gmail.com>
2025-05-24 15:46:37 +0900

    new components added

---
+++ force-app/main/default/lwc/myFamilyTreeHomePage/myFamilyTreeHomePage.js
@@ -0,0 +1,3 @@
+export default class myFamilyTreeHomePage extends LightningElement {
+    // Add properties and methods to handle component behavior and data
+}
---
+++ force-app/main/default/lwc/myPorch/myPorch.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class MyPorch extends LightningElement {}
